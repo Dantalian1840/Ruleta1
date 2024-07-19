88ae4964514cb52656b77fd18069f0eefd3db579
Sergio <sergioalejandrogarzon.emat@gmail.com>
2024-07-19 14:06:17 -0500

    Entorno de trabajo

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Documentacion de la prueba técnica de la ruleta para Mezubo.com')
+    .setDescription('Rutas para el uso del servidor')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
